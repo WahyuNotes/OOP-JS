4c0b0ace9c79706c3bf6bc977d8bebc14c519b4a
wramadhan <wahyuramadhan0549@gmail.com>
2022-11-17 10:44:51 +0700

    2

--- bagian1.js
+++ bagian1.js
@@ -11,5 +11,24 @@ baru.firstname = "eko"; //isi objeknya
 const baru2 = new CobaMakan();
 baru2.lastname = "oki";
 
-console.log(baru);
-console.log(baru2);
+// console.log(baru);
+// console.log(baru2);
+
+// Contoh lain
+function Testing() {
+  this.firstname = "";
+  this.lastname = "";
+
+  //kalau mau memasukkan function ke dalam function
+  this.fungsi = function (name) {
+    console.info(
+      `Hello ${name}, my name is ${this.firstname} ${this.lastname}`
+    );
+  };
+}
+const value = new Testing();
+value.firstname = "Eko";
+value.lastname = "Khannedy";
+value.fungsi("budi");
+
+console.log(value);
